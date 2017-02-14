36401f27455a7a637d3467690993cec7283256f8
michelleskim <Michelle@michelles-mbp.dhcp.wustl.edu>
2017-02-13 19:14:33 -0600

    1

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+<!-- The overlay -->
+<div id="myNav" class="overlay">
+
+<!-- Button to close the overlay navigation -->
+<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+<!-- Overlay content -->
+<div class="overlay-content">
+  <a href="index.html#home" class="nav">Home</a>
+  <a href="index.html" class="nav">Intro</a>
+  <a href="about.html" class="nav">About</a>
+</div>
+
+</div>
+<!-- Use any element to open/show the overlay navigation menu -->
+
+<div class="nav-container">
+<a href="index.html"> <i class="fa fa-chevron-left" aria-hidden="true" id="back-home"></i> </a>
+<span onclick="openNav()" class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></span>
+</div>
+?>
---
+++ scripts.js
@@ -0,0 +1,30 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
