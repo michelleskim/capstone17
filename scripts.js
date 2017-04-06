2d7a7daa7708d5f92b5295fb1a1bdd01ecd24fdc
michelleskim <Michelle@MacBook-Pro.local>
2017-04-06 18:54:40 -0500

    Hopefully fixed

--- scripts.js
+++ scripts.js
@@ -28,3 +28,44 @@ window.onclick = function(event) {
     }
   }
 }
+
+jQuery(function($){
+
+  var currentpanel=0;
+
+  $(document).on("scroll",handlescroll);
+
+  function handlescroll(e){
+    var st=$(document).scrollTop();
+    var panel=Math.floor(st/200);
+
+    if (panel!=currentpanel){
+      currentpanel=Math.min(6,panel);
+
+      showpanel(currentpanel);
+    }
+  }
+
+
+  function showpanel(n){
+    //scroll the the correct panel
+    $("#panel").css({
+      "top":(n*-100)+"%"
+    })
+    //update nav dots
+    $(".breadcrumbs-intro i").removeClass("fa-circle").addClass("fa-circle-o");
+    $(".breadcrumbs-intro").children().eq(n).children("i").removeClass("fa-circle-o").addClass("fa-circle");
+    console.log(n);
+  }
+
+  $(".breadcrumbs-intro a").on("click",handledotclick);
+
+  function handledotclick(e){
+    var n=$(this).index();
+    showpanel(n);
+    e.preventDefault();
+  }
+
+  $(".fa-chevron-down").on("click",handledotclick);
+
+});
