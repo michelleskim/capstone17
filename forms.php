8b81a2b29a1c05edd93394ab66207893fe2df5fa
michelleskim <Michelle@MacBook-Pro.local>
2017-04-06 21:20:00 -0500

    test-2

---
+++ forms.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Data Gathered</title>
+  </head>
+  <body>
+    <?php
+    $words = htmlspecialchars($_POST['words']);
+
+    echo $words, '',
+
+    ?>
+  </body>
+<html>
